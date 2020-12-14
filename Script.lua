f4ed1cb832dbfef40bdca9fe48dfeeb483e0a546
markdenardo <mark.denardo@gmail.com>
2020-12-14 18:57:06 -0500

    first commit

---
+++ Script.lua
@@ -0,0 +1,22 @@
+function makePart()
+ -- declaration of local variables i and rand
+ local i = 0
+ local rand = math.random(1000)
+ -- variable assignment of new Part instance
+ local part = Instance.new('Part')
+ -- assigns part properties
+ part.Parent = game.Workspace
+ part.Size = Vector3.new(100, 1, 100)
+ part.Position = Vector3.new(rand, rand, rand)
+ part.Color = Color3.new(1,0,1)
+ part.Anchored = false
+ -- increments i counter and prints
+ i = i + 1
+ print(i)
+ -- one second wait at end of code block pauses loop when executed
+ wait(1)
+end
+--while true is true (true is always true) execute the loop
+while true do 
+  makePart()
+end
