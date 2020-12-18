214d333b694011bc30291f4b3c716a6589a25c7c
markdenardo <mark.denardo@gmail.com>
2020-12-18 13:28:04 -0500

    iteration

--- Script.lua
+++ Script.lua
@@ -1,12 +1,12 @@
 function makePart()
  -- declaration of local variables i and rand
  local i = 0
- local rand = math.random(1000)
+ local rand = math.random(-1000, 1000)
  -- variable assignment of new Part instance
  local part = Instance.new('Part')
  -- assigns part properties
  part.Parent = game.Workspace
- part.Size = Vector3.new(100, 1, 100)
+ part.Size = Vector3.new(rand, rand, rand)
  part.Position = Vector3.new(rand, rand, rand)
  part.Color = Color3.new(1,0,1)
  part.Anchored = false
